d67740d198e64a02bb454d17142359e27cee8ad6
G-Vale <gabrielcrc.pj@gmail.com>
2022-11-28 11:59:10 -0300

    ECOPMMER TEST CELL

--- Js/ecommerce.js
+++ Js/ecommerce.js
@@ -107,3 +107,51 @@ $('document').ready(function(){
    
   return false;
 });
+
+/* BUSCAR */
+$(document).ready(function () {
+
+  // navSearch show/hide
+  function navSearch() {
+    $('.nav-search').on('click', function () {
+      $('.search-block').fadeIn(350);
+    });
+    $('.search-close').on('click', function () {
+      $('.search-block').fadeOut(350);
+    });
+  }
+  navSearch();
+})
+
+
+// jQuery Smooth Scroll
+$(".navbar a").on("click", function (e) {
+  if (this.hash !== "") {
+    e.preventDefault();
+
+    const hash = this.hash;
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top
+      },
+      800
+    );
+  }
+});
+
+
+$(document).ready(function(){
+
+  $(window).scroll(function(){
+       if ($(this).scrollTop() > 100) {
+$('#scrollToTop').fadeIn();
+} else {
+$('#scrollToTop').fadeOut();
+}
+  });
+
+  $('#scrollToTop').click(function(){
+$('html, body').animate({scrollTop : 0},800);
+return false;
+  }); 
+});
